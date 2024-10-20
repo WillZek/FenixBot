41537f316d322a60a9752c49da1c89baa6da4446
Niño Piña <ninopina10@gmail.com>
2024-10-20 08:32:22 -0600

    Actualizar

---
+++ handler.js
@@ -0,0 +1,100 @@
+import { smsg } from './lib/simple.js'
+import { format } from 'util' 
+import { fileURLToPath } from 'url'
+import path, { join } from 'path'
+import { unwatchFile, watchFile } from 'fs'
+import chalk from 'chalk'
+import fetch from 'node-fetch'
+
+const { proto } = (await import('@whiskeysockets/baileys')).default
+const isNumber = x => typeof x === 'number' && !isNaN(x)
+const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function () {
+clearTimeout(this)
+resolve()
+}, ms))
+
+export async function handler(chatUpdate) {
+this.msgqueque = this.msgqueque || []
+if (!chatUpdate)
+return
+    this.pushMessage(chatUpdate.messages).catch(console.error)
+let m = chatUpdate.messages[chatUpdate.messages.length - 1]
+if (!m)
+return;
+if (global.db.data == null)
+await global.loadDatabase()       
+try {
+m = smsg(this, m) || m
+if (!m)
+return
+m.exp = 0
+m.yenes = false
+try {
+let user = global.db.data.users[m.sender]
+if (typeof user !== 'object')
+
+global.db.data.users[m.sender] = {}
+if (user) {
+if (!isNumber(user.exp))
+user.exp = 0
+if (!isNumber(user.yenes))
+user.yenes = 10
+if (!('muto' in user))
+user.muto = false
+if (!('premium' in user)) 
+user.premium = false
+if (!user.premium) 
+user.premiumTime = 0
+if (!('registered' in user))
+user.registered = false
+if (!user.registered) {
+if (!('name' in user))
+user.name = m.name
+if (!isNumber(user.age))
+user.age = -1
+if (!isNumber(user.regTime))
+user.regTime = -1
+}
+if (!isNumber(user.afk))
+user.afk = -1
+if (!('afkReason' in user))
+user.afkReason = ''
+if (!('banned' in user))
+user.banned = false
+if (!('useDocument' in user))
+user.useDocument = false
+if (!isNumber(user.level))
+user.level = 0
+if (!isNumber(user.bank))
+user.bank = 0
+} else
+                global.db.data.users[m.sender] = {
+exp: 0,
+yenes: 10,
+muto: false,
+registered: false,
+name: m.name,
+age: -1,
+regTime: -1,
+afk: -1,
+afkReason: '',
+banned: false,
+useDocument: false,
+bank: 0,
+level: 0,
+}
+let chat = global.db.data.chats[m.chat]
+if (typeof chat !== 'object')
+global.db.data.chats[m.chat] = {}
+if (chat) {
+if (!('isBanned' in chat))
+chat.isBanned = false
+if (!('welcome' in chat))
+chat.welcome = true
+if (!('audios' in chat))
+chat.audios = false
+if (!('detect' in chat))
+chat.detect = true
+if (!('onlyLatinos' in chat))
+chat.onlyLatinos = true 
+if (!('antiBot' in chat))
