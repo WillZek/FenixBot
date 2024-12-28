f2373b1ffbb9010aaa1f1ce47d8c2874722504d8
Niño Piña <ninopina10@gmail.com>
2024-12-28 10:37:49 -0600

    Actualizar Fenix•code.js

--- plugins/Fenix•code.js
+++ plugins/Fenix•code.js
@@ -91,3 +91,84 @@ let handler = async (m, { conn: _conn, args, usedPrefix, command, isOwner }) => 
                 m.react(rwait);
                 let codeBot = await conn.requestPairingCode(cleanedNumber);
                 codeBot = codeBot?.match(/.{1,4}/g)?.join("-") || codeBot
+                let txt = ` –  *S E R B O T  -  S U B B O T*\n\n`;
+                txt += `┌  👑  *Usa este Código para convertirte en un Sub Bot*\n`;
+                txt += `│  🌠  Pasos\n`;
+                txt += `│  🌠  *1* : Haga click en los 3 puntos\n`;
+                txt += `│  🌠  *2* : Toque dispositivos vinculados\n`;
+                txt += `│  🌠  *3* : Selecciona *Vincular con el número de teléfono*\n`; 
+                txt += `└  🌠  *4* : Escriba el Codigo\n\n`;
+                txt += `*👑Nota:* Este Código solo funciona en el número en el que se solicitó\n\n> *Sigan El Canal* https://whatsapp.com/channel/0029VakfOZfHFxP7rNrUQk2d`;
+                await parent.reply(m.chat, txt, m, rcanal);
+                m.react(done);
+                await parent.reply(m.chat, codeBot, m, rcanal);
+                rl.close();
+            }, 3000);
+        }
+
+        conn.isInit = false;
+        let isInit = true;
+
+        async function connectionUpdate(update) {
+            const { connection, lastDisconnect, isNewLogin, qr } = update;
+            if (isNewLogin) conn.isInit = true;
+            const code = lastDisconnect?.error?.output?.statusCode || lastDisconnect?.error?.output?.payload?.statusCode;
+            if (code && code !== DisconnectReason.loggedOut && conn?.ws.socket == null) {
+                let i = global.conns.indexOf(conn);
+                if (i < 0) return console.log(await creloadHandler(true).catch(console.error));
+                delete global.conns[i];
+                global.conns.splice(i, 1);
+
+                if (code !== DisconnectReason.connectionClosed) {
+                    parent.sendMessage(m.chat, { text: "Conexión perdida.." }, { quoted: m });
+                }
+            }
+
+            if (global.db.data == null) loadDatabase();
+
+            if (connection == 'open') {
+                conn.isInit = true;
+                global.conns.push(conn);
+                await parent.reply(m.chat, args[0] ? 'Conectado con exito' : '*Conectado exitosamente*, Sub Bot de (*CrowBot*👑)\n\n*Nota:* Esto es temporal\nSi el Bot principal se reinicia o se desactiva, todos los sub bots también lo harán\n\nEl número del bot puede cambiar, síguenos para que estés al tanto de los siguientes números:\n*-* https://whatsapp.com/channel/0029VakfOZfHFxP7rNrUQk2d', m, rcanal);
+                await sleep(5000);
+                if (args[0]) return;
+
+                await parent.reply(conn.user.jid, `La siguiente vez que se apague el bot o se desconecte envía el siguiente mensaje para iniciar sesión sin utilizar otro código `, m, rcanal);
+                await parent.sendMessage(conn.user.jid, { text: usedPrefix + command + " " + Buffer.from(fs.readFileSync("./CrowJadiBot/" + authFolderB + "/creds.json"), "utf-8").toString("base64") }, { quoted: m });
+            }
+        }
+
+        setInterval(async () => {
+            if (!conn.user) {
+                try { conn.ws.close(); } catch { }
+                conn.ev.removeAllListeners();
+                let i = global.conns.indexOf(conn);
+                if (i < 0) return;
+                delete global.conns[i];
+                global.conns.splice(i, 1);
+            }
+        }, 60000);
+
+        let handler = await import('../handler.js');
+        let creloadHandler = async function (restatConn) {
+            try {
+                const Handler = await import(`../handler.js?update=${Date.now()}`).catch(console.error);
+                if (Object.keys(Handler || {}).length) handler = Handler;
+            } catch (e) {
+                console.error(e);
+            }
+            if (restatConn) {
+                try { conn.ws.close(); } catch { }
+                conn.ev.removeAllListeners();
+                conn = makeWASocket(connectionOptions);
+                isInit = true;
+            }
+
+            if (!isInit) {
+                conn.ev.off('messages.upsert', conn.handler);
+                conn.ev.off('connection.update', conn.connectionUpdate);
+                conn.ev.off('creds.update', conn.credsUpdate);
+            }
+
+            conn.handler = handler.handler.bind(conn);
+            conn.connectionUpdate = connectionUpdate.bind(conn);
