c4cb7e413d059687972601245116fec07c4851ee
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-14 10:15:26 -0600

    Actualizar sunlight.js

--- sunlight.js
+++ sunlight.js
@@ -131,7 +131,7 @@ let handler = async (m, { conn, usedPrefix, command, args, usedPrefix: _p, __dir
 await conn.sendMessage(m.chat, { text: menuA + '\n' + menuB, mentions: mentionedJid }, { quoted: m, contextInfo: fkontak })
 
 } catch (e) {
-    await conn.sendMessage(m.chat, { text: `\nTítulo`, footer: 'Error al ejecutar el comando. #report ' + usedPrefix + command }, { quoted: m })
+await conn.sendMessage(m.chat, { text: menuA + '\n' + menuB, mentions: mentionedJid }, { quoted: m, contextInfo: fkontak })
     console.log(e)        
 }}
 
